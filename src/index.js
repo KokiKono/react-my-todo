71695ea1eae88212d29cd465600d2608353e76a9
kokikono <kk.k.0421@outlook.jp>
2017-08-21 00:08:49 +0900

    Fixed Router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import {render} from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import createHistory from 'history/createBrowserHistory'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
 import reducers from './Reducers';
-import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 const history = createHistory();
 const middleware = routerMiddleware(history);
@@ -24,7 +22,7 @@ render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <div>
-        <Route exact path="/" component={App}></Route>
+        <App history={history}/>
       </div>
     </ConnectedRouter>
   </Provider>,
